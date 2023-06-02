ab5fd06964aed8120c8e778218b02513b5f4f9ee
stephenlambertfs <stephenolambert@gmail.com>
2023-06-02 12:44:41 -0600

    added solution code

--- 05_Auth/server/app.py
+++ 05_Auth/server/app.py
@@ -10,7 +10,6 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_cors import CORS
 from models import db, User
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,33 +20,67 @@ db.init_app(app)
 
 api = Api(app)
 CORS(app)
+#1. ✅ python -c 'import os; print(os.urandom(16))'
+#Used to hash the session data
+app.secret_key = b'\xb8C\xack"}]c_\xb7\xf0\xcdng\xe7\xdf'
 
-# app.secret_key = 'BAD_SECRET_KEY'
-# python -c 'import os; print(os.urandom(16))'
+# 2.✅ Create a Login route
+    # 3.1 Create a login class that inherits from Resource
+    # 3.2 Use api.add_resource to add the '/login' path
+    # 3.3 Build out the post method
+        # 3.3.1 convert the request from json and select the user name sent form the client. 
+        # 3.3.2 Use the name to query the user with a .filter
+        # 3.3.3 If found set the user_id to the session hash
+        # 3.3.4 convert the user to_dict and send a response back to the client 
+class Users(Resource):
+     def post(self):
+        form_json = request.get_json()
+        new_user = User(
+            name=form_json['name'],
+        )
+        db.session.add(new_user)
+        print(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+        return make_response(
+            new_user.to_dict(),
+            201,
+        )
+api.add_resource(Users, '/login')
 
-# Storing user specific data
-# session['data'] will be different per cookie
-# session.get('data') to get the data 
-# How can use this for user login?
-
-# Lets create a login route that will check if the user exist and
-# Save it to session
-
-# Create a logout route now! set session to None
-
-# Use @app.before_request!
-
-class All_Customers(Resource):
+# 3.✅ Create a check login route that will save the user to the session
+class check_login(Resource):
     def get(self):
-        json_obj = { "Type" : "Get"}
-        res = make_response(jsonify(json_obj),200)
-        return res
+        user_id = session.get("user_id")
 
-    def post(self):
-        json_obj = { "Type" : "Post"}
-        res = make_response(jsonify(json_obj),200)
-        return res
-api.add_resource(All_Customers, '/students')
+        if user_id:
+            user = User.query.filter(User.id == session["user_id"]).first()
+            return make_response(
+                user.to_dict(),
+                200,
+            )
+api.add_resource(check_login, '/check_login')
+
+# 4. Create a logout route now! set session to None
+class logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response(
+            "Logged out",
+            200,
+        )
+api.add_resource(logout, '/logout')
+# Use @app.before_request, to run a function before every request
+
+@app.before_request
+def check_session():
+    print(session)
+    if session.get("user_id") is None:
+        session["user_id"] = None
+    else:
+        print("User is logged in")
+        print(session["user_id"])
+
 
 if __name__ == '__main__':
     app.run(port=5555)
--- 05_Auth/server/models.py
+++ 05_Auth/server/models.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
-
 metadata = MetaData(naming_convention={
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -17,8 +16,21 @@ db = SQLAlchemy(metadata=metadata)
 class User(db.Model,SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String, unique = True)
+    name = db.Column(db.String, unique = True)
+    # _password_hash = db.Column(db.String)
     
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # @hybrid_property
+    # def password_hash(self):
+    #     return self._password_hash
+    
+    # @password_hash.setter
+    # def password(self, password):
+    #     password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+    #     self._password_hash = password_hash.decode('utf-8')
+
+    # def autheticate(self, password):
+    #     return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
 
